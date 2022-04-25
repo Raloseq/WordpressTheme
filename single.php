810c9da11eb52a3691ec67965ce8514b43f3c479
Raloseq <45914275+Raloseq@users.noreply.github.com>
2022-04-25 14:55:50 +0200

    added title and author to single post

--- single.php
+++ single.php
@@ -2,6 +2,10 @@
 <?php while( have_posts(  ) ): the_post(  )?>
     <section class="app-page__content app-page__content-single">
         <div class="post-content">
+            <div class="post-content__heading">
+                <h3><?php the_title( ) ?></h3>
+                <h3 class="post-content__heading-author">Author: <?php the_author( ) ?></h3>
+            </div>
             <?php the_content( ) ?>
         </div>
         <div class="post-sidebar">
